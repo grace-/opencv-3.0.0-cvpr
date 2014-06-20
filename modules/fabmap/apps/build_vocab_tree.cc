de70578d26b525320625d6af97ebb357faf29060
Prasanna Krishnasamy <pkrishnasamy@magicleap.com>
2014-06-20 12:05:56 -0700

    Moving the find cmake files to top level
    * made stub fabmap

---
+++ modules/fabmap/apps/build_vocab_tree.cc
@@ -0,0 +1,12 @@
+#include <opencv2/highgui,hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/contrib.hpp>
+
+int main(int argc, char *argv[]) {
+  // read images
+  //
+
+  // build vocabulary tree
+  //
+  FabMap2 fabmap_obj;
+}
