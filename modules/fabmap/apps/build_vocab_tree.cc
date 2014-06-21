8235da34fc0c6fea24e84ffaabff158458f3d461
Prasanna Krishnasamy <pkrishnasamy@magicleap.com>
2014-06-20 19:49:56 -0700

    Making executable to build a vocabulary tree
    - just skeleton

--- modules/fabmap/apps/build_vocab_tree.cc
+++ modules/fabmap/apps/build_vocab_tree.cc
@@ -1,12 +1,34 @@
-#include <opencv2/highgui,hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/contrib.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/contrib/contrib.hpp>
+
+#include <cstdio>
+
+using cv::Mat;
+using cv::imshow;
+using cv::waitKey;
 
 int main(int argc, char *argv[]) {
   // read images
-  //
+  cv::VideoCapture cap(0);
+  if (!cap.isOpened()) {
+    printf("Opening the default camera did not succeed\n"); 
+    return -1;
+  }
+
+  for (;;) {
+    Mat frame;
+    cap >> frame;
+    imshow("input RGB image", frame);
+    if (waitKey(30) >= 0)
+      break;
+  }
 
   // build vocabulary tree
   //
-  FabMap2 fabmap_obj;
+  // FabMap2 fabmap_obj;
+  // std::vector<cv::Mat> query_img_descriptors;
+  // fabmap_obj.addTraining(query_img_descriptors);
+
+  return 0;
 }
